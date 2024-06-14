14274b7cf449c1e8be2e75aa74aadd554cf60755
iesblucasolv <lucas.o.ferreira@iesb.edu.br>
2024-06-14 15:25:10 -0300

    inserindo views

--- app/src/main/java/com/example/pi3apdm/MainActivity.java
+++ app/src/main/java/com/example/pi3apdm/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pi3apdm;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void btnOnClickViewPrimeiroAcesso(View view){
+        setContentView(R.layout.primeiro_acesso);
     }
 }
